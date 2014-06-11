81197bb42a58d20fddee0a86d4fe392048a122e0
Senghoo Kim <shkdmb@gmail.com>
2014-06-11 16:25:58 +0800

    let clippy-say show flycheck errors

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -1,9 +1,19 @@
 (add-hook 'after-init-hook 'global-flycheck-mode)
 
 ;; Override default flycheck triggers
-(setq flycheck-check-syntax-automatically '(save idle-change mode-enabled)
-      flycheck-tip-avoid-show-func nil
-      flycheck-idle-change-delay 0.8)
-(global-set-key (kbd "C-c C-n") 'flycheck-tip-cycle)
+
+
+(defun my-flycheck-mode-hook()
+  (setq flycheck-check-syntax-automatically '(save idle-change mode-enabled)
+        flycheck-tip-avoid-show-func nil
+        flycheck-idle-change-delay 0.8
+        flycheck-display-errors-function 'flycheck-clippy-show-error-messages))
+
+(add-hook 'flycheck-mode-hook 'my-flycheck-mode-hook)
+
+(defun flycheck-clippy-show-error-messages (errors)
+  (-when-let (messages (-keep #'flycheck-error-message errors))
+    (clippy-say
+     (mapconcat 'identity messages "\n"))))
 
 (provide 'init-flycheck)
