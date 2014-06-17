0d383df89bbdde8a26a83608d1af6060a13e9374
Senghoo Kim <shkdmb@gmail.com>
2014-06-17 18:01:41 +0800

    add package autoinstall

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -25,10 +25,66 @@
 
 ;;; On-demand installation of packages
 
+(defvar prelude-packages '(ace-jump-mode
+                           auto-complete
+                           auto-complete-clang
+                           auto-complete-clang-async
+                           color-theme
+                           color-theme-sanityinc-tomorrow
+                           dired+
+                           direx
+                           discover-my-major
+                           exec-path-from-shell
+                           expand-region
+                           flycheck
+                           fuzzy
+                           helm
+                           helm-flycheck
+                           helm-projectile
+                           helm-projectile-all
+                           highlight-indentation
+                           highlight-symbol
+                           ibuffer-vc
+                           jedi
+                           jedi-direx
+                           magit
+                           markdown-mode
+                           markdown-mode+
+                           multiple-cursors
+                           popup
+                           pos-tip
+                           powerline
+                           projectile
+                           pyvenv
+                           rainbow-delimiters
+                           slime
+                           undo-tree
+                           visual-regexp
+                           web-mode
+                           wgrep
+                           xcscope
+                           yaml-mode
+                           yasnippet)
+  "A list of packages to ensure are installed at launch.")
+
+
+(defun prelude-packages-installed-p ()
+  (loop for p in prelude-packages
+        when (not (package-installed-p p)) do (return nil)
+        finally (return t)))
+
+(unless (prelude-packages-installed-p)
+  ;; check for new packages (package versions)
+  (message "%s" "Emacs Prelude is now refreshing its package database...")
+  (package-refresh-contents)
+  (message "%s" " done.")
+  ;; install the missing packages
+  (dolist (p prelude-packages)
+    (when (not (package-installed-p p))
+      (package-install p))))
 
 (package-initialize)
 
 
 (provide 'init-elpa)
 
-;;; init-elpa.el ends here
