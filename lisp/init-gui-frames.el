d662c794bcf6fe665e6cb47c872e7d930d22bec0
Senghoo Kim <shkdmb@gmail.com>
2014-06-18 10:54:47 +0800

    fix scroll-bar-mode function not find error

--- lisp/init-gui-frames.el
+++ lisp/init-gui-frames.el
@@ -6,10 +6,8 @@
 (setq inhibit-startup-screen t)
 (setq inhibit-startup-echo-area-message t)
 (tool-bar-mode -1)
-(scroll-bar-mode -1)
-;;----------------------------------------------------------------------------
-;; Show a marker in the left fringe for lines not in the buffer
-;;----------------------------------------------------------------------------
+(if (fboundp 'scroll-bar-mode)
+    (scroll-bar-mode -1))
 (setq indicate-empty-lines t)
 (global-set-key (kbd "M-RET") 'toggle-frame-fullscreen)
 (provide 'init-gui-frames)
