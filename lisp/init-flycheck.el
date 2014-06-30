c9e07cbb92563c9f5a8f0e9dbbd628e1d3c18e2a
Senghoo Kim <shkdmb@gmail.com>
2014-06-30 09:25:16 +0800

    fix clippy-say break issue

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -2,17 +2,29 @@
 
 (defun my-flycheck-mode-hook()
   (setq flycheck-check-syntax-automatically '(save idle-change mode-enabled)
-        flycheck-tip-avoid-show-func nil
         flycheck-idle-change-delay 0.8
         flycheck-display-errors-function 'flycheck-clippy-show-error-messages
+        flycheck-indication-mode nil
         flycheck-flake8-maximum-line-length 120))
+
 
 (add-hook 'flycheck-mode-hook 'my-flycheck-mode-hook)
 
+(defun flycheck-clippy-show-error-auto-newline (str)
+  (if (< (string-width str) 60)
+      str
+      (concat (substring str 0 60)
+              "\n"
+              (flycheck-clippy-show-error-auto-newline (substring str 60)))))
+
 (defun flycheck-clippy-show-error-messages (errors)
-  (-when-let (msg (mapconcat 'identity (-keep #'flycheck-error-message errors) "\n"))
+  (-when-let (msg (mapconcat 'identity (mapcar
+                                        'flycheck-clippy-show-error-auto-newline
+                                        (-keep #'flycheck-error-message errors) )
+                             "\n\n"))
     (if (popup-live-p ac-menu)
         (message  msg )
         (clippy-say msg))))
+
 
 (provide 'init-flycheck)
